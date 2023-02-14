7c325cc9df1b9ab08661af6b59ac440dbcd83660
JoshDPT <jjewcu@gmail.com>
2023-02-14 15:43:24 -0500

    Add inputs for Profile Form

---
+++ client/components/ProfileForm.tsx
@@ -0,0 +1,16 @@
+
+
+// add components as children
+export default function ProfileForm() {
+  return (
+    <form className="space-y-8 divide-y divide-gray-200">
+      <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+        {/* NameInput */}
+        {/* Pronouns Input */}
+        {/* About Me Input */}
+        {/* Preferred Date Form */}
+        {/*  Save Button */}
+      </div>
+    </form>
+  )
+}
