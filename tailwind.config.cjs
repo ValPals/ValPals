221dc7d7cfd80f877fef277f842b242e7ac44186
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-02-15 12:10:36 -0500

    merging from dev

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
-  theme: { 
-    fontFamily: {
-    poppins: ['Poppins', 'sans-serif'],
-    roboto: ['Roboto', 'sans-serif'],
-  },
-    extend: {colors: {
-      'error-red': '#FF0000',
-      white: '#FFFFFF',
-      black: '#000000',
-      grey: '#6b7280',
-      pink: '#b784a7',
-    },
-    backgroundImage: {
-      kubernetes: "url('./client/assets/kubernetes.jpg')",
-    },
-    height: {
-      128: '32rem',
-    },
-    width: {
-      120: '20rem',
-    },
-  },
+  content: [
+    './index.html',
+    "./client/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
   plugins: [
-    // require('tailwindcss/forms'),
+    //require('tailwindcss/forms'),
   ],
 }
